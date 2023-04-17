749014cd73cde84e1fe2d0b82c9538b4c85fbaeb
j-nowland <127156607+j-nowland@users.noreply.github.com>
2023-04-18 11:28:22 +1200

    quokka

--- src/pair_coding.js
+++ src/pair_coding.js
@@ -1,18 +1,9 @@
-// Create a function that returns true if the first array values is subst of the second array values
-// Otherwise return false
-// arraySubset ([3,2,5][5,3,7,9,2])
-
-function arraySubset(arr1, arr2) {
-    let counter = 0
-    for(let i =0, i < arr1.length, i++) {
-        if(arr2.includes(arr1[i])){
-            counter++
-        }
-if (counter === arr1.length)
-        return true
-    } else {
-        return false
-    }
+// write a function that converts an object into an array.
+//
 
 
+function toArray(obj) {
+    return Object.entries(obj)
 }
+
+console.log(toArray({classroom: 215, poscode: 7010}))
